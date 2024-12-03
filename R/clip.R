598cd5288248d289a201b9a803c26eef1077c92d
Jose Samos <jsamos@ugr.es>
2024-12-03 06:37:03 +0100

    Clip functions

--- R/clip.R
+++ R/clip.R
@@ -1,38 +1,106 @@
-#' Clip a multipoligon vector layer based on a polygon
+#' Clip a Vector Layer with a Polygon
 #'
-#' We will use this function only if function `clip_vector()` gives an error.
-#' The error I have found is `ParseException: Unknown WKB type 12`, it has to do
-#' with type *MULTIPOLYGON*.
+#' This function clips a vector layer (e.g., points, lines, polygons) using a polygon layer.
+#' It handles CRS (Coordinate Reference System) transformations automatically if necessary,
+#' ensuring the output is in the same CRS as the input polygon.
 #'
-#' The solution adopted is based on the one described in
-#' https://gis.stackexchange.com/questions/389814/r-st-centroid-geos-error-unknown-wkb-type-12
+#' @param vector An `sf` object representing the vector layer to be clipped.
+#' @param polygon An `sf` object representing the polygon layer used for clipping.
 #'
-#' We must ensure that the layer to be cut has the correct encoding.
+#' @return An `sf` object containing the features of the input `vector` that intersect with the `polygon`.
+#' The output will be in the CRS of the `polygon`, and it will retain all attributes of the input `vector`.
 #'
-#' Project the result to the CRS of the clipping polygon.
+#' @examples
 #'
-#' @param vector A `sf` multipoligon vector layer.
-#' @param polygon A `sf` polygon layer.
+#' # Example data
+#' vector <- sf::st_as_sf(data.frame(
+#'   id = 1:3,
+#'   geometry = sf::st_sfc(
+#'     st_point(c(0.5, 0.5)),
+#'     st_point(c(1.5, 1.5)),
+#'     st_point(c(2.5, 2.5))
+#'   )
+#' ), crs = 4326)
 #'
-#' @return A `sf` vector layer.
+#' polygon <- sf::st_as_sf(data.frame(
+#'   id = 1,
+#'   geometry = sf::st_sfc(st_polygon(list(rbind(
+#'     c(0, 0), c(2, 0), c(2, 2), c(0, 2), c(0, 0)
+#'   ))))
+#' ), crs = 4326)
+#'
+#' # Clip the vector using the polygon
+#' clipped_vector <- clip_vector(vector, polygon)
+#'
+#' @export
+clip_vector <- function(vector, polygon) {
+  if (!sf::st_crs(vector) == sf::st_crs(polygon)) {
+    polygon <- sf::st_transform(polygon, sf::st_crs(vector))
+  }
+  res <- sf::st_intersection(vector, polygon)
+  res <- res[names(vector)]
+  if (!sf::st_crs(vector) == sf::st_crs(polygon)) {
+    res <- sf::st_transform(res, sf::st_crs(polygon))
+  }
+  res
+}
+
+
+#' Safely Clip a Multipolygon Vector Layer
+#'
+#' This function clips a `MULTIPOLYGON` vector layer using a polygon layer, handling specific
+#' issues that might arise with geometries encoded incorrectly or containing unknown WKB types.
+#' It serves as a fallback when the `clip_vector()` function fails due to errors like
+#' `ParseException: Unknown WKB type 12`, which is associated with *MULTIPOLYGON* types.
+#'
+#' The function ensures that the input layer is correctly encoded as `MULTIPOLYGON` and
+#' uses GDAL utilities for re-encoding if necessary. The output is projected to the CRS
+#' of the clipping polygon.
+#'
+#' This solution is inspired by a discussion on handling WKB type errors in R:
+#' <https://gis.stackexchange.com/questions/389814/r-st-centroid-geos-error-unknown-wkb-type-12>.
+#'
+#' @param vector A `sf` multipolygon vector layer to be clipped.
+#' @param polygon A `sf` polygon layer used as the clipping geometry.
+#'
+#' @return A `sf` vector layer with the clipped geometries.
 #'
 #' @family transformation functions
 #'
 #' @examples
-#' #
+#'
+#' # Example data
+#' vector <- sf::st_as_sf(data.frame(
+#'   id = 1:2,
+#'   geometry = sf::st_sfc(
+#'     st_polygon(list(rbind(c(0, 0), c(2, 0), c(2, 2), c(0, 2), c(0, 0)))),
+#'     st_polygon(list(rbind(c(1, 1), c(3, 1), c(3, 3), c(1, 3), c(1, 1))))
+#'   )
+#' ), crs = 4326)
+#'
+#' polygon <- sf::st_as_sf(data.frame(
+#'   id = 1,
+#'   geometry = sf::st_sfc(st_polygon(list(rbind(
+#'     c(1, 1), c(2, 1), c(2, 2), c(1, 2), c(1, 1)
+#'   ))))
+#' ), crs = 4326)
+#'
+#' # Clip the vector using the polygon
+#' result <- safe_clip_multipolygon(vector, polygon)
+#' print(result)
 #'
 #' @export
-clip_multipoligon <- function(vector, polygon) {
+safe_clip_multipolygon <- function(vector, polygon) {
   tryCatch({
-    v <- sf::st_cast(vector, to = "MULTIPOLYGON")
+    v <- sf::st_cast(vector, "MULTIPOLYGON")
     clip_vector(v, polygon)
-  },
-  error = function(e) {
+  }, error = function(e) {
     f <- tempfile(fileext = ".gpkg")
-    g <- tempfile(fileext = ".gpkg")
     sf::st_write(vector, f, quiet = TRUE)
+    g <- tempfile(fileext = ".gpkg")
     gdalUtilities::ogr2ogr(f, g, f = "GPKG", nlt = "MULTIPOLYGON")
     v <- sf::st_read(g, quiet = TRUE)
     clip_vector(v, polygon)
   })
 }
+
--- tests/testthat/test-clip.R
+++ tests/testthat/test-clip.R
@@ -1,3 +1,159 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("clip_vector handles intersecting geometries", {
+  vector <- sf::st_as_sf(data.frame(
+    id = 1:3,
+    geometry = sf::st_sfc(sf::st_point(c(0.5, 0.5)), sf::st_point(c(1.5, 1.5)), sf::st_point(c(2.5, 2.5)))
+  ), crs = 4326)
+
+  polygon <- sf::st_as_sf(data.frame(id = 1, geometry = sf::st_sfc(st_polygon(list(
+    rbind(c(0, 0), c(2, 0), c(2, 2), c(0, 2), c(0, 0))
+  )))), crs = 4326)
+
+  result <- clip_vector(vector, polygon)
+
+  expect_s3_class(result, "sf")
+  expect_equal(nrow(result), 2)
 })
+
+test_that("clip_vector handles disjoint geometries", {
+  vector <- sf::st_as_sf(data.frame(
+    id = 1:3,
+    geometry = sf::st_sfc(sf::st_point(c(10, 10)), sf::st_point(c(11, 11)), sf::st_point(c(12, 12)))
+  ), crs = 4326)
+
+  polygon <- sf::st_as_sf(data.frame(
+    id = 1,
+    geometry = sf::st_sfc(sf::st_polygon(list(
+      rbind(c(0, 0), c(2, 0), c(2, 2), c(0, 2), c(0, 0))
+    )))
+  ), crs = 4326)
+
+  result <- clip_vector(vector, polygon)
+
+  expect_s3_class(result, "sf")
+  expect_equal(nrow(result), 0)
+})
+
+test_that("clip_vector handles CRS transformations", {
+  vector <- sf::st_as_sf(data.frame(
+    id = 1:3,
+    geometry = sf::st_sfc(sf::st_point(c(0.5, 0.5)), sf::st_point(c(1.5, 1.5)), sf::st_point(c(2.5, 2.5)))
+  ), crs = 4326)
+
+  polygon <- sf::st_as_sf(data.frame(
+    id = 1,
+    geometry = sf::st_sfc(sf::st_polygon(list(
+      rbind(c(0, 0), c(200000, 0), c(200000, 200000), c(0, 200000), c(0, 0))
+    )))
+  ), crs = 32630)
+
+  result <- clip_vector(vector, polygon)
+
+  expect_s3_class(result, "sf")
+  expect_equal(nrow(result), 2)
+  expect_equal(sf::st_crs(result), sf::st_crs(polygon))
+})
+
+
+test_that("safe_clip_multipolygon handles correct clipping", {
+  vector <- sf::st_as_sf(data.frame(
+    id = 1:2,
+    geometry = sf::st_sfc(sf::st_polygon(list(
+      rbind(c(0, 0), c(2, 0), c(2, 2), c(0, 2), c(0, 0))
+    )), sf::st_polygon(list(
+      rbind(c(1, 1), c(3, 1), c(3, 3), c(1, 3), c(1, 1))
+    )))
+  ), crs = 4326)
+
+  polygon <- sf::st_as_sf(data.frame(
+    id = 1,
+    geometry = sf::st_sfc(sf::st_polygon(list(
+      rbind(c(1, 1), c(2, 1), c(2, 2), c(1, 2), c(1, 1))
+    )))
+  ), crs = 4326)
+
+  result <- safe_clip_multipolygon(vector, polygon)
+
+  expect_s3_class(result, "sf")
+  expect_equal(nrow(result), 2)
+})
+
+test_that("safe_clip_multipolygon handles invalid MULTIPOLYGON encoding",
+          {
+            vector <- sf::st_as_sf(data.frame(
+              id = 1:2,
+              geometry = sf::st_sfc(
+                sf::st_geometrycollection(list(sf::st_polygon(list(
+                  rbind(c(0, 0), c(2, 0), c(2, 2), c(0, 2), c(0, 0))
+                )))),
+                sf::st_geometrycollection(list(sf::st_polygon(list(
+                  rbind(c(1, 1), c(3, 1), c(3, 3), c(1, 3), c(1, 1))
+                ))))
+              )
+            ), crs = 4326)
+
+            polygon <- sf::st_as_sf(data.frame(
+              id = 1,
+              geometry = sf::st_sfc(sf::st_polygon(list(
+                rbind(c(1, 1), c(2, 1), c(2, 2), c(1, 2), c(1, 1))
+              )))
+            ), crs = 4326)
+
+            result <- safe_clip_multipolygon(vector, polygon)
+
+            expect_s3_class(result, "sf")
+            expect_equal(nrow(result), 1)
+          })
+
+test_that("safe_clip_multipolygon handles CRS transformations", {
+  vector <- sf::st_as_sf(data.frame(
+    id = 1:2,
+    geometry = sf::st_sfc(sf::st_polygon(list(
+      rbind(c(0, 0), c(2, 0), c(2, 2), c(0, 2), c(0, 0))
+    )), sf::st_polygon(list(
+      rbind(c(1, 1), c(3, 1), c(3, 3), c(1, 3), c(1, 1))
+    )))
+  ), crs = 4326)
+
+  polygon <- sf::st_as_sf(data.frame(
+    id = 1,
+    geometry = sf::st_sfc(sf::st_polygon(list(
+      rbind(
+        c(100000, 100000),
+        c(200000, 100000),
+        c(200000, 200000),
+        c(100000, 200000),
+        c(100000, 100000)
+      )
+    )))
+  ), crs = 32630)
+
+  result <- safe_clip_multipolygon(vector, polygon)
+
+  expect_s3_class(result, "sf")
+  expect_equal(nrow(result), 0)
+  expect_equal(sf::st_crs(result), sf::st_crs(polygon))
+})
+
+test_that("safe_clip_multipolygon returns empty for disjoint geometries",
+          {
+            vector <- sf::st_as_sf(data.frame(
+              id = 1:2,
+              geometry = sf::st_sfc(sf::st_polygon(list(
+                rbind(c(10, 10), c(12, 10), c(12, 12), c(10, 12), c(10, 10))
+              )), sf::st_polygon(list(
+                rbind(c(15, 15), c(18, 15), c(18, 18), c(15, 18), c(15, 15))
+              )))
+            ), crs = 4326)
+
+            polygon <- sf::st_as_sf(data.frame(
+              id = 1,
+              geometry = sf::st_sfc(sf::st_polygon(list(
+                rbind(c(0, 0), c(5, 0), c(5, 5), c(0, 5), c(0, 0))
+              )))
+            ), crs = 4326)
+
+            result <- safe_clip_multipolygon(vector, polygon)
+
+            expect_s3_class(result, "sf")
+            expect_equal(nrow(result), 0)
+          })
